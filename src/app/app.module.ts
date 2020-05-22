94e00c2ffd4b98f1ae016ca33b3c2bd98c18a646
unknown <jhoncardenas.11@gmail.com>
2020-05-21 19:53:51 -0500

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// firebase
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
+
+// components
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
